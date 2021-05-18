5b6d1edd0cf915f6e42097a9414a288c191e6e8b
zzb66666666x <18767066589@163.com>
2021-05-18 20:32:33 +0800

    updated

--- data/generate_feature_graph_clique.py
+++ data/generate_feature_graph_clique.py
@@ -1,38 +1,68 @@
-
-# coding: utf-8
-
 import networkx as nx
+import numpy as np
 import pandas as pd
-from itertools import combinations
+from tqdm import tqdm
+#import matplotlib.pyplot as plt
 
 
-df_train = pd.read_csv('train.csv').fillna("")
-df_test = pd.read_csv('test.csv').fillna("")
-len_train = df_train.shape[0]
-
-df = pd.concat([df_train[['question1', 'question2']], df_test[['question1', 'question2']]], axis=0)
-
-
-G = nx.Graph()
-edges = [tuple(x) for x in df[['question1', 'question2']].values]
-G.add_edges_from(edges)
-
-map_label = dict(((x[0], x[1])) for x in df[['question1', 'question2']].values)
-map_clique_size = {}
-cliques = sorted(list(nx.find_cliques(G)), key=lambda x: len(x))
-for cli in cliques:
-    for q1, q2 in combinations(cli, 2):
-        if (q1, q2) in map_label:
-            map_clique_size[q1, q2] = len(cli)
-        elif (q2, q1) in map_label:
-            map_clique_size[q2, q1] = len(cli)
-
-df['clique_size'] = df.apply(lambda row: map_clique_size.get((row['question1'], row['question2']), -1), axis=1)
-
-
-df[['clique_size']][:len_train].to_csv('train_feature_graph_clique.csv', index=False)
-df[['clique_size']][len_train:].to_csv('test_feature_graph_clique.csv', index=False)
+path=''
+train=pd.read_csv(path+'train.csv')
+test=pd.read_csv(path+'test.csv')
+y_train=train.is_duplicate.values
+data=pd.concat([train[['question1','question2']],test[['question1','question2']]]).reset_index()
+data.drop('index',axis=1,inplace=True)
+q_list={}
+dd=data.values
+for i in tqdm(np.arange(data.values.shape[0])):
+#for i in np.arange(dd.shape[0]):
+    q1,q2=dd[i]
+    if q_list.setdefault(q1,[i])!=[i]:
+        q_list[q1].append(i)
+    if q_list.setdefault(q2,[i])!=[i]:
+        q_list[q2].append(i)
+data['question1_link']=data.question1.map(q_list)
+data['question2_link']=data.question2.map(q_list)
 
 
 
+data['adj_node']=data.apply(lambda x:x['question1_link']+x['question2_link'],axis=1)
 
+
+
+G=nx.Graph()
+cnt=0
+for i,adj_list in tqdm(enumerate(data.adj_node.values)):
+#for i,adj_list in enumerate(data.adj_node.values):
+    edges=[(i,item) for item in adj_list if item !=i]
+    if edges==[]:
+#        if cnt%100000==0:
+#            print("ignore cnts %d" %cnt)
+        cnt+=1
+    else:
+        G.add_edges_from(edges)  
+print("ignore cnts %d" %cnt)
+
+print('begin train')
+cnt=0
+max_clique=np.zeros(data.shape[0])
+for clique in tqdm(nx.find_cliques(G)):
+#for clique in nx.enumerate_all_cliques(G):
+    if cnt%100000==0:
+        print("deal cnts %d" %cnt)
+    len_clique=len(clique)
+    for item in clique:
+        max_clique[item]=max(max_clique[item],len_clique)
+    cnt+=1
+
+
+print("totally max_clique %d" %cnt)
+
+n_train = train.shape[0]
+train_clique_data = max_clique[0:n_train]
+test_clique_data = max_clique[n_train:]
+
+pd_train_clique = pd.DataFrame(train_clique_data,columns=['graph_clique_feat'])
+pd_train_clique.to_csv('train_feature_graph_clique.csv', index=False)
+
+pd_test_clique = pd.DataFrame(test_clique_data,columns=['graph_clique_feat'])
+pd_test_clique.to_csv('test_feature_graph_clique.csv', index=False)
